4a826280512be0c4ff4f6fb69919012ecab7dfd2
Aleksey Krylov <elsite2015@yandex.ru>
2020-11-21 04:49:28 +0400

    4.3.6. Math.abs [#411755]

---
+++ test/ru/job4j/condition/ChessBoardTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ChessBoardTest {
+    @Test
+    public void wayIs2() {
+        int way = ChessBoard.way(0, 0, 2, 2);
+        assertThat(way, is(2));
+    }
+
+    @Test
+    public void wayIs1() {
+        int way = ChessBoard.way(1, 1, 2, 0);
+        assertThat(way, is(1));
+    }
+
+    @Test
+    public void wayIs3() {
+        int way = ChessBoard.way(0, 3, 3, 0);
+        assertThat(way, is(3));
+    }
+
+    @Test
+    public void wayIs0() {
+        int way = ChessBoard.way(0, 3, 1, 3);
+        assertThat(way, is(0));
+    }
+}
