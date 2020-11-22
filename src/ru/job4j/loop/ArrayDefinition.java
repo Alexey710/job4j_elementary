c975286ee56ef61149a540bcde64fdf1796ce044
Aleksey Krylov <elsite2015@yandex.ru>
2020-11-22 10:31:45 +0400

    6.0.2. Заполнение массива. [#411777]

--- src/ru/job4j/loop/ArrayDefinition.java
+++ src/ru/job4j/loop/ArrayDefinition.java
@@ -2,11 +2,21 @@ package ru.job4j.loop;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
+        /*short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
         System.out.println(ages.length);
         System.out.println(surnames.length);
-        System.out.println(prices.length);
+        System.out.println(prices.length);*/
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Donald Trump";
+        names[2] = "Alexey Smirnov";
+        names[3] = "Jhon Gold";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
