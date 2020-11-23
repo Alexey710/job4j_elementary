3770b3492faaeb6c923e62593423e24aaa2b0311
Aleksey Krylov <elsite2015@yandex.ru>
2020-11-23 07:18:58 +0400

    5.7. Ипотека [#411855]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,14 +4,11 @@ public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
         double remainder = amount + percent / 100 * amount;
-        if (remainder - salary <= 0) {
-            return 1;
-        }
-        while (remainder >= 0) {
+        do {
             remainder = remainder - salary;
             remainder = remainder + percent / 100 * remainder;
             year++;
-        }
+        } while (remainder >= 0);
         return year;
     }
 }
