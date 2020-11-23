286be356579146ba917b2b8584dfa36eabf89e89
Aleksey Krylov <elsite2015@yandex.ru>
2020-11-23 06:48:42 +0400

    5.4. Протеиновая диета [#411853]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -2,12 +2,10 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        int progressPowerDeadliftPerMonthOfNic = nik;
-        int progressPowerDeadliftPerMonthOfIvan = ivan;
         int month = 0;
-        while (progressPowerDeadliftPerMonthOfNic > progressPowerDeadliftPerMonthOfIvan) {
-            progressPowerDeadliftPerMonthOfNic = progressPowerDeadliftPerMonthOfNic * 2;
-            progressPowerDeadliftPerMonthOfIvan = progressPowerDeadliftPerMonthOfIvan * 3;
+        while (nik > ivan) {
+            nik = nik * 2;
+            ivan = ivan * 3;
             month++;
         }
         return month;
--- test/ru/job4j/loop/FitnessTest.java
+++ test/ru/job4j/loop/FitnessTest.java
@@ -3,7 +3,8 @@ package ru.job4j.loop;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class FitnessTest {
     @Test
